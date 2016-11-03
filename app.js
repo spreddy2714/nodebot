cda07c3fe6ba64a4014c640e4fbd109df223162f
Siva Prathap Reddy <Siva Prathap Reddy>
2016-11-04 00:47:26 +0530

    commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var restify = require('restify');
 var builder = require('botbuilder');
-
+var restify = require('restify');
+var process = require('./env.js');
+var assert = require('assert');
+var RestifyClient = require('./client.js');
 //=========================================================
 // Bot Setup
 //=========================================================
@@ -8,9 +10,13 @@ var builder = require('botbuilder');
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
-  
+
+RestifyClient.login({}, function (err, req, res, obj) {
+    console.log('%j', obj);
+    // session.beginDialog('/profile');
+});
 // Create chat bot
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
@@ -18,6 +24,7 @@ var connector = new builder.ChatConnector({
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
+
 
 //=========================================================
 // Bots Dialogs
@@ -28,24 +35,46 @@ bot.dialog('/', intents);
 
 intents.matches(/^Change name/i, [
     function (session) {
-        session.beginDialog('/profile');
+        appRestifyClient.login({}, function (err, req, res, obj) {
+            console.log('%j', obj);
+            session.beginDialog('/profile');
+        });
     },
     function (session, results) {
         session.send('Ok.. I have changed your name to %s', session.userData.name);
     }
 ]);
 
-intents.onDefault([function (session, args, next) {
-        if(!session.userData.name){
-            session.beginDialog('/profile');
-        }
-        else {
-            next();
-        }
+intents.matches(/^Picture/i, [
+    function (session) {
+        session.beginDialog('/picture');
     },
     function (session, results) {
-        session.send('Hello %s', session.userData.name);    
+        session.send('How is the picture like?');
     }
+]);
+
+intents.matches(/^login/i, [
+    function (session) {
+        session.beginDialog('/login');
+    },
+    function (session, results) {
+        session.send('How is the picture like?');
+    }
+]);
+
+
+intents.onDefault([function (session, args, next) {
+    if (!session.userData.name) {
+        session.beginDialog('/profile');
+    }
+    else {
+        next();
+    }
+},
+function (session, results) {
+    session.send('Hello %s', session.userData.name);
+}
 ]);
 
 bot.dialog('/profile', [
@@ -54,6 +83,45 @@ bot.dialog('/profile', [
     },
     function (session, results) {
         session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
+
+bot.dialog('/picture', [
+    function (session) {
+        var msg = new builder.Message(session)
+            .textFormat(builder.TextFormat.xml)
+            .attachments([
+                new builder.HeroCard(session)
+                    .title("Hero Card")
+                    .subtitle("Space Needle")
+                    .text("The <b>Space Needle</b> is an observation tower in Seattle, Washington, a landmark of the Pacific Northwest, and an icon of Seattle.")
+                    .images([
+                        builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
+                    ])
+                    .tap(builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Space_Needle"))
+            ]);
+        session.endDialog(msg);
+    },
+    function (session, results) {
+        session.endDialog();
+    }
+]);
+
+
+bot.dialog('/login', [
+    function (session) {
+        var signinCard = new builder.SigninCard(session)
+            .button("Login", "http://questfy.ubiqfy.com/#/login")
+            .text("Login")
+        var msg = new builder.Message(session)
+            .attachments([
+                signinCard
+            ]);
+        console.log(signinCard.toAttachment());
+        session.endDialog(msg);
+    },
+    function (session, results) {
         session.endDialog();
     }
 ]);
